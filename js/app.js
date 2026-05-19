1af5ab93201ff575e9eb458d44945cf555b6d0a7
Altjin09 <p.altjin09@gmail.com>
2026-05-20 04:51:11 +0800

    fix: GATEWAY URL-г api-gateway болгов

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // ============ CONFIG ============
-const GATEWAY = 'https://product-order-service-22ayo.ondigitalocean.app';
+const GATEWAY = 'https://api-gateway-ot9qp.ondigitalocean.app'; 
 const SOAP_URL = 'https://soap-auth-service-87mrv.ondigitalocean.app/ws';
 // ============ AUTH HELPERS ============
 function getToken()    { return localStorage.getItem('soa_token') || ''; }
